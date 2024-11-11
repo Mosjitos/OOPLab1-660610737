b3def9f58ad2d5d5b39adf893e155944db927b7c
Mosjitos <kamolrat_saeboon@cmu.ac.th>
2024-11-11 23:09:49 +0700

    Create array Keep which can assign size

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter size: ");
+        Scanner Get = new Scanner(System.in);
+        int N = Get.nextInt();
+        int[] Keep = new int[N];
+    }
+}
